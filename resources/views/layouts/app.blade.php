5508dc9c50048cb58a74b1dd40c0f046d8fa45fd
sabreenMohammed107 <sabreenm312@gmail.com>
2023-05-22 15:53:40 +0200

    first upload

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,52 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>  مزارع جمال جمعة</title>
+
+   <!-- Scripts -->
+   <script src="{{ asset('js/app.js') }}" defer></script>
+
+   <!-- Fonts -->
+   <link rel="dns-prefetch" href="https://fonts.gstatic.com">
+   <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
+
+   <!-- Styles -->
+   <link href="{{ asset('css/appAr.css') }}" rel="stylesheet">
+</head>
+<body style="direction: rtl">
+    <div id="app">
+        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+            <div class="container">
+                <a class="navbar-brand" href="{{ url('/') }}">
+
+                   <img alt="Logo" src="{{ asset('img/logo.PNG') }}" class="max-h-50px logo-default "
+                    style="height: 120px" />
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="navbar-nav me-auto">
+
+                    </ul>
+
+                    <!-- Right Side Of Navbar -->
+
+                </div>
+            </div>
+        </nav>
+
+        <main class="py-4">
+            @yield('content')
+        </main>
+    </div>
+</body>
+</html>
